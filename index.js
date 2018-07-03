0f08e1ab15814a7980b5cd648fd23d5070836115
Alex Caswell <caswellyatsenko@gmail.com>
2018-07-03 04:19:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-sayHiToGradma(string) {
+sayHiToGradma(string) 
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   }
-}
